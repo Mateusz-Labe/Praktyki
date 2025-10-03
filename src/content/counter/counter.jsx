de1d87e0d64367be101665e9e07e2c0fc42786ed
Mateusz Labe <mati.labenda@gmail.com>
2025-10-03 15:26:11 +0200

    Counter

--- src/content/counter/counter.jsx
+++ src/content/counter/counter.jsx
@@ -6,10 +6,12 @@ const Counter = () => {
     const saved = localStorage.getItem("clicks");
     return saved ? parseInt(saved, 10) : 0;
   });
+  const [clicks2, setClicks2] = useState(0);
 
   const decrease = () => setClicks(c => c - 1);
   const reset = () => setClicks(0);
   const increase = () => setClicks(c => c + 1);
+  const increase2 = () => setClicks2(c => c + 1);
 
  
   useEffect(() => {
@@ -20,9 +22,12 @@ const Counter = () => {
     <div>
       <div className='counter-box'>
         <h1 className='counter'>{clicks}</h1>
-        <button onClick={decrease}>Decrease</button>
-        <button onClick={reset}>Reset</button>
-        <button onClick={increase}>Increase</button>
+        <button className='decres' onClick={decrease}>Decrease</button>
+        <button className='reset' onClick={reset}>Reset</button>
+        <button className='incres' onClick={increase}>Increase</button>
+      </div>
+      <div className="counter2">
+        <button onClick={increase2}>{clicks2}</button>
       </div>
     </div>
   );
